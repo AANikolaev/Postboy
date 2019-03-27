115fe9e2a9fab09cab21d23410ffc4e4b1541b3a
Alexander Nikolaev <pchelas.07@gmail.com>
2019-03-27 15:43:20 +0200

    create nav menu

--- app/src/main/java/nikolaev/postboy/view/activities/MainActivity.kt
+++ app/src/main/java/nikolaev/postboy/view/activities/MainActivity.kt
@@ -2,12 +2,18 @@ package nikolaev.postboy.view.activities
 
 import android.content.DialogInterface
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.GravityCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 import nikolaev.postboy.R
 import nikolaev.postboy.databinding.ActivityMainBinding
 import nikolaev.postboy.util.*
@@ -15,13 +21,14 @@ import nikolaev.postboy.view.base.BaseActivity
 import nikolaev.postboy.view.interfaces.IRouter
 import nikolaev.postboy.viewmodel.MainViewModel
 
-class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>(), IRouter<Int> {
+class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>(), IRouter<Int>,
+        NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var navController: NavController
     private var preloader: AlertDialog? = null
 
     override fun obtainViewModel(): MainViewModel =
-        ViewModelProviders.of(this).get(MainViewModel::class.java)
+            ViewModelProviders.of(this).get(MainViewModel::class.java)
 
     override fun getContentViewLayoutId(): Int = R.layout.activity_main
 
@@ -38,12 +45,12 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>(), IRouter
         viewModel.errorDialogEvent.observe(this, Observer<Event<ErrorDialogModel>> {
             it?.getContentIfNotHandled()?.let { errorDialogModel ->
                 showMessageDialogWithSingleAction(this,
-                    errorDialogModel.errorMessage
-                        ?: getString(R.string.sing_up_basics_error_dialog_message_default),
-                    getString(R.string.message_dialog_default_cancle_button_text),
-                    DialogInterface.OnClickListener { dialog, _ ->
-                        dialog.cancel()
-                    })
+                        errorDialogModel.errorMessage
+                                ?: getString(R.string.sing_up_basics_error_dialog_message_default),
+                        getString(R.string.message_dialog_default_cancle_button_text),
+                        DialogInterface.OnClickListener { dialog, _ ->
+                            dialog.cancel()
+                        })
             }
         })
 
@@ -55,11 +62,36 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>(), IRouter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setSupportActionBar(toolbar)
-
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
     override fun moveToNextFragment(i: Int) {
         navController.navigate(i)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_history -> {
+                Toast.makeText(this, "in progress", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
