d865b2a91b81ae6f589655af9714b94329b8a7c4
Nikolaev Alexandr <alexandr.nikolaev.dev@gmail.com>
2024-06-24 16:16:38 +0300

    added adb

--- app/src/main/java/anicode/postboy/view/fragments/TabRootFragment.kt
+++ app/src/main/java/anicode/postboy/view/fragments/TabRootFragment.kt
@@ -1,5 +1,6 @@
 package anicode.postboy.view.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,8 +11,20 @@ import com.google.android.material.tabs.TabLayout
 import anicode.postboy.R
 import anicode.postboy.view.activities.MainActivity
 import anicode.postboy.view.adapter.SampleFragmentPagerAdapter
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+import com.google.android.ump.ConsentInformation
+import com.google.android.ump.ConsentRequestParameters
+import com.google.android.ump.UserMessagingPlatform
+import java.util.concurrent.atomic.AtomicBoolean
 
 class TabRootFragment : Fragment() {
+
+    private lateinit var mAdView: AdView
+
+    private lateinit var consentInformation: ConsentInformation
+    private var isMobileAdsInitializeCalled = AtomicBoolean(false)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,14 +33,46 @@ class TabRootFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_tab_root, container, false)
 
-        val viewPager = view.findViewById(R.id.viewpager) as ViewPager
+        val viewPager: ViewPager = view.findViewById(R.id.viewpager)
         viewPager.adapter = SampleFragmentPagerAdapter(fragmentManager!!)
 
-        val slidingTabs = view.findViewById(R.id.sliding_tabs) as TabLayout
+        val slidingTabs: TabLayout = view.findViewById(R.id.sliding_tabs)
         slidingTabs.post { slidingTabs.setupWithViewPager(viewPager) }
 
         (activity as MainActivity).supportActionBar?.hide()
 
+        mAdView = view.findViewById(R.id.adView)
+
+        val params = ConsentRequestParameters.Builder().setTagForUnderAgeOfConsent(false).build()
+
+        consentInformation = UserMessagingPlatform.getConsentInformation(view.context)
+        consentInformation.requestConsentInfoUpdate(this.activity!!, params, {
+            UserMessagingPlatform.loadAndShowConsentFormIfRequired(
+                this.activity!!
+            ) {
+                if (consentInformation.canRequestAds()) {
+                    initializeMobileAdsSdk(view.context)
+                }
+            }
+        }, {})
+
+        if (consentInformation.canRequestAds()) {
+            initializeMobileAdsSdk(view.context)
+        }
+
+
         return view
+    }
+
+    private fun initializeMobileAdsSdk(context: Context) {
+        if (isMobileAdsInitializeCalled.get()) {
+            return
+        }
+        isMobileAdsInitializeCalled.set(true)
+
+        MobileAds.initialize(context) {}
+
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 }
